954db9d8fcbf351c41e988b4099707ee56550b8a
PJGitLan <pj.lanneer@gmail.com>
2021-08-21 11:02:03 +0200

    first version done

--- app/src/main/java/com/example/adbquicktoggle/MyTileService.kt
+++ app/src/main/java/com/example/adbquicktoggle/MyTileService.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
-import android.util.Log
 import android.widget.Toast
 import java.io.DataOutputStream
 import java.io.IOException
@@ -16,59 +15,46 @@ class MyTileService : TileService() {
 
     override fun onStartListening() {
         super.onStartListening()
-        setState()
-        Log.v("Basic", "in startListening after state set")
+        setTileState(getADBState())
     }
 
-    private fun setState(){
-        if (adbActive()){
+    private fun setTileState(adbState : Boolean){
+        if (adbState){
             qsTile.state = Tile.STATE_ACTIVE
-            Log.v("Basic", "in if setstate => state_active")
         }
-        else if (!adbActive()) {
+        else if (!adbState) {
             qsTile.state = Tile.STATE_INACTIVE
-            Log.v("Basic", "in if else setstate => state_active")
 
         }
-    }
-
-    override fun onTileAdded() {
-        super.onTileAdded()
-        //check adb state
-        //update state
-        qsTile.state = Tile.STATE_ACTIVE
-
-        //update looks
         qsTile.updateTile()
     }
+
+    /*override fun onTileAdded() {
+        super.onTileAdded()
+        setState();
+    }*/
 
     override fun onClick() {
         super.onClick()
-        val adbAct = adbActive()
-        var toastmsg = "Has to be dynamic."
-        Log.d("ADB_onClick", "state: $adbAct")
-        if (adbAct){
-            //TODO("Change adb state.")
-            qsTile.state = Tile.STATE_INACTIVE //Don't do this in if else but after
-            setAdb(false)
-        }
-        else if(!adbAct){
-            qsTile.state = Tile.STATE_ACTIVE //Don't do this in if else but after
-            setAdb(true)
-        }
-        //TODO("Print the adb state as a toast message.")
-        /*val toast = */ Toast.makeText(context, toastmsg, Toast.LENGTH_SHORT).show()
-        //toast.show()
-        qsTile.updateTile()
+        setADBState(!getADBState())
+        val adbState = getADBState()
+        setTileState(adbState)
+        showADBState(adbState)
     }
 
-    private fun adbActive(): Boolean {
-        val state = Settings.Global.getInt(context.getContentResolver(), Settings.Global.ADB_ENABLED, 0)
-        //Log.d("ADB_STATE", "state: $state")
+    private fun showADBState(currState : Boolean) {
+        var toastmsg = "ADB is "
+        toastmsg += if(currState) "active!" else "inactive!"
+
+        Toast.makeText(context, toastmsg, Toast.LENGTH_LONG).show()
+    }
+
+    private fun getADBState(): Boolean {
+        val state = Settings.Global.getInt(context.contentResolver, Settings.Global.ADB_ENABLED, 0)
         return state == 1
     }
 
-    private fun setAdb(on: Boolean){
+    private fun setADBState(on: Boolean){
         val onInt = if (on) 1 else 0
         try {
             val su = Runtime.getRuntime().exec("su")
